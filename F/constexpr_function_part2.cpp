b45a060aa94bce06e3b6eafe66d34c08e05c68b5
Dwi Arifin <mr.dnarifin@gmail.com>
2025-07-08 19:25:58 +0700

    Constexpr Functions

---
+++ F/constexpr_function_part2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int getValue(int x)
+{
+    return x;
+}
+
+// This function can be evaluated at runtime
+// WHen evaluated at compile-time, the function will produce a compilation error
+// because the call to getValue(x) cannot be resolved at compile-time
+
+constexpr int foo(int x)
+{
+    if (x < 0) return 0;    // needed prior to adoption of P2448R1 in C++23
+    return getValue(x);     // call to non-constexpr function here
+}
+
+int main()
+{
+    int x { foo(5)};            // okay: will evaluate at runtime
+    constexpr int y { foo(5)};  // compile error: foo(5) can't evaluate at compile-time
+}
---
+++ F/function_constexpr.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+constexpr double calcCircumference(double radius)
+{
+    constexpr double pi { 3.14159265359};
+    return 2.0 * pi * radius;
+}
+
+int main()
+{
+    constexpr double circumference { calcCircumference(3.0)};   // compile error
+    std::cout << "Our circle has circumference " << circumference << "\n";
+
+    return 0;
+}
